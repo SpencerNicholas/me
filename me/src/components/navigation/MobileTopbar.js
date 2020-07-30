38e4e322acb4a4827d5a51df9933ac7b8b1023b2
Spencer Nicholas <spencer.nicholas@hotmail.co.uk>
2020-07-30 15:21:04 +0100

    Added socials to mobile
    Added social links to mobile nav bar

--- me/src/components/navigation/MobileTopbar.js
+++ me/src/components/navigation/MobileTopbar.js
@@ -3,6 +3,12 @@ import { useSelector, useDispatch } from 'react-redux';
 import { setPage, selectPage, selectHover, setHover, setMenuOpen, selectMenu } from './navigation'
 import Menu from '@material-ui/icons/Menu';
 
+import LinkedIn from '@material-ui/icons/LinkedIn';
+import Instagram from '@material-ui/icons/Instagram';
+import Twitter from '@material-ui/icons/Twitter';
+import GitHub from '@material-ui/icons/GitHub';
+
+
 const MobileTopbar = ({ }) => {
   const hovering = useSelector(selectHover);
   const page = useSelector(selectPage);
@@ -22,7 +28,7 @@ const MobileTopbar = ({ }) => {
   };
 
   const mobileTopbarExpandedStyle = {
-    height: "60vh",
+    height: "70vh",
     width: "100%",
     position: "fixed",
     backgroundColor: "#428e92",
@@ -56,6 +62,26 @@ const MobileTopbar = ({ }) => {
     fontWeight: "bold",
   }
 
+  const footerStyle = {
+    color: 'white',
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: '3vh',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-evenly'
+  }
+
+  const iconStyle = {
+    fontSize: '32px',
+  }
+
+  const iconHovered = {
+    fontSize: '32px',
+    color: 'red',
+    cursor: 'pointer',
+  }
+
   const menuItems = [
     { text: "About", active: 1 },
     { text: "Experience", active: 2 },
@@ -71,20 +97,20 @@ const MobileTopbar = ({ }) => {
     <div>
       {!menu ? (
         <div style={mobileTopbarStyle}>
-             {menuItems.map((item, i) => (
-              <div key={i} style={pageTitle}>
-                {item.active === (page) ? (
-                  <div>
-                    {item.text}                  
-                    <Menu style={{fontSize: '32px', position: 'absolute', right: '5%'}} onClick={() => {dispatch(setMenuOpen(true))}}></Menu>
-                    </div>
-                ):(<div></div>)}
-                  </div>))}
-                  </div>
+          {menuItems.map((item, i) => (
+            <div key={i} style={pageTitle}>
+              {item.active === (page) ? (
+                <div>
+                  {item.text}
+                  <Menu style={{ fontSize: '32px', position: 'absolute', right: '5%' }} onClick={() => { dispatch(setMenuOpen(true)) }}></Menu>
+                </div>
+              ) : (<div></div>)}
+            </div>))}
+        </div>
       ) : (
           <div style={mobileTopbarExpandedStyle}>
             {menuItems.map((item, i) => (
-              <div key={i} style={menuItemStyle} onClick={() => { dispatch(setPage(item.text));  dispatch(setMenuOpen(false)) } }>
+              <div key={i} style={menuItemStyle} onClick={() => { dispatch(setPage(item.text)); dispatch(setMenuOpen(false)) }}>
                 {item.active === (page) ? (
                   <div style={activeMenuItemStyle}>
                     {item.text}
@@ -104,7 +130,39 @@ const MobileTopbar = ({ }) => {
                   )}
               </div>
             ))}
+            <div style={footerStyle}>
+              <div onMouseOver={() => { dispatch(setHover("LinkedIn")) }} onMouseLeave={() => { dispatch(setHover()) }} onClick={() => { window.open("https://linkedin.com/in/spencer-nicholas", "_blank") }}>
+                {(hovering) === 6 ? (
+                  <LinkedIn style={iconHovered} ></LinkedIn>
+                ) : (
+                    <LinkedIn style={iconStyle} ></LinkedIn>
+                  )}
+              </div>
+              <div onMouseOver={() => { dispatch(setHover("Instagram")) }} onMouseLeave={() => { dispatch(setHover()) }} onClick={() => { window.open("https://www.instagram.com/spencerndev/?hl=en", "_blank") }}>
+                {(hovering) === 7 ? (
+                  <Instagram style={iconHovered}></Instagram>
+                ) : (
+                    <Instagram style={iconStyle}></Instagram>
+                  )}
+              </div>
+              <div onMouseOver={() => { dispatch(setHover("Twitter")) }} onMouseLeave={() => { dispatch(setHover()) }} onClick={() => { window.open("https://twitter.com/SpencerNDev", "_blank") }}>
+                {(hovering) === 8 ? (
+                  <Twitter style={iconHovered}></Twitter>
+                ) : (
+                    <Twitter style={iconStyle}></Twitter>
+                  )}
+              </div>
+              <div onMouseOver={() => { dispatch(setHover("GitHub")) }} onMouseLeave={() => { dispatch(setHover()) }} onClick={() => { window.open("https://github.com/SpencerNicholas", "_blank") }}>
+                {(hovering) === 9 ? (
+                  <GitHub style={iconHovered}></GitHub>
+                ) : (
+                    <GitHub style={iconStyle}></GitHub>
+                  )}
+              </div>
+            </div>
           </div>
+
+
 
         )}
     </div>
